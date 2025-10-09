ff58be62ff55e43bc00063e4151a02a10da49969
sooooscode <127713796+sooooscode@users.noreply.github.com>
2025-10-09 14:39:59 +0900

    [BE-soo] Create index.php

---
+++ src/index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * File: index.php
+ * Author: Yeonsu Kim (Backend Developer)
+ * Date: 2025-10-
+ * Role: 프로젝트의 시작점 (홈페이지 0)
+ * 모든 환경 초기화, 사용자로부터 핵심 입력(지역/기간)을 받아 세션에 저장, 기능 네비게이션 제공
+ */
+
+?>
