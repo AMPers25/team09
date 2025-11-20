233dc696c762f23090be931919f625b54d175e17
sooooscode <sooooscode@gmail.com>
2025-11-21 01:05:18 +0900

    fix(dbdump): DB 사용 추가

--- sql/dbdump.sql
+++ sql/dbdump.sql
@@ -15,6 +15,9 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+-- DB 사용
+USE team09;
+
 --
 -- Table structure for table `AirQuality`
 --
